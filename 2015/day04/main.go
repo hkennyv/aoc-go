661d2163d6e7a963a85c8d48e013ec707e344ae5
Kenny Huynh <hkennyv@gmail.com>
2022-09-05 23:35:14 -0700

    add concurrent solution for 2015 day04
    works and is a little faster than the non-concurrent one, but still has
    some bugs

--- 2015/day04/main.go
+++ 2015/day04/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 )
 
 func main() {
@@ -44,23 +45,82 @@ func lowestNumberHash(key string, prefix string) int {
 	}
 }
 
-// DOESN'T WORK :(
-// can't figure out how to only return 1 value from goroutine
+// this is my best attempt at a shitty concurrent solution in go. this one uses
+// a worker pool pattern to spawn a pool of 50 workers that will receive a
+// number and calculate the hash. if it finds a match, it passes it to a
+// channel to be returned
+//
+// it performs a little better than the non-concurrent solution @ 2.875s vs
+// 3.566s for the concurrent on my machine
+//
+// it still has some bugs and if we increase the size of the buffered input
+// channel, it crashes because it will find >1 matches and try to send them
+// to the found channel
 func lowestNumberHash2(key string, prefix string) int {
-	first := make(chan int, 1)
+	var wg sync.WaitGroup
 
-	for i := 0; i < 100000000; i++ {
-		go func(n int) {
-			data := []byte(fmt.Sprintf("%s%d", key, n))
+	numWorkers := 50
+
+	input := make(chan int, 100000)
+	found := make(chan int)
+
+	stop := make(chan interface{})
+
+	// spawn numWorkers workers who will run until they receive a stop signal
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			worker(key, prefix, input, found, stop)
+		}()
+	}
+
+	return receiver(numWorkers, input, found, stop)
+}
+
+// this worker consumes input from the input channel indefinitely until a stop
+// signal is sent. once it finds a hash with matching prefix, it sends the
+// result over the found channel
+func worker(key string, prefix string, input <-chan int, found chan<- int, stop <-chan interface{}) {
+	for {
+		select {
+		case i := <-input:
+			data := []byte(fmt.Sprintf("%s%d", key, i))
 			hash := md5.Sum(data)
 
 			s := fmt.Sprintf("%x", hash)
 
 			if strings.HasPrefix(s, prefix) {
-				first <- n
+				found <- i
 			}
-		}(i)
+		case <-stop:
+			return
+		}
 	}
+}
 
-	return <-first
+// the receiver will check the found channel for any results and continue to
+// increment and put numbers into the input channel for the workers to consume
+// once it receives a value from the found channel, it sends the stop signals
+// and returns the result
+func receiver(numWorkers int, input chan<- int, found <-chan int, stop chan<- interface{}) int {
+	var i int
+
+	for {
+		select {
+		case res := <-found:
+			// send stop to all workers, this can still produce a race
+			// condition though if the workers try to send another result to
+			// the found channel before they get stopped
+			for i := 0; i < numWorkers; i++ {
+				stop <- struct{}{}
+			}
+
+			return res
+		default:
+			input <- i
+		}
+		i++
+	}
 }
