6781f1bc78c7d1a0f42f9b9dd3335367b3ad8b24
Kenny Huynh <hkennyv@gmail.com>
2022-12-05 23:19:59 -0800

    ✍️ simplify 2022 day 6

--- 2022/day06/main.go
+++ 2022/day06/main.go
@@ -19,26 +19,9 @@ func parseInput(fp string) []byte {
 }
 
 func findStartOfPacket(bs []byte, n int) int {
-	buf := make([]byte, n)
-
-	// prefill buf
-	for i := 0; i < n; i++ {
-		buf[i] = bs[i]
-	}
-
-	// first check
-	if isUnique(buf, n) {
-		return n
-	}
-
-	// check rest
 	for i := n; i < len(bs); i++ {
-		for j := 0; j < n; j++ {
-			buf[j] = bs[i-(n-1)+j]
-		}
-
-		if isUnique(buf, n) {
-			return i + 1
+		if isUnique(bs[i-n:i], n) {
+			return i
 		}
 	}
 
